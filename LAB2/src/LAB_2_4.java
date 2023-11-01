a97b858bec36caad63d27fa968c5e89bcdfa828a
KasirTiavina2110 <tiavinakasir@gmail.com>
2023-11-01 18:25:32 -0400

    Create LAB_2_4.java

---
+++ LAB2/src/LAB_2_4.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class LAB_2_4 {
+    public static void main(String[] args) {
+        
+        
+        final float surfaceRouleau = 50f;
+        String hauteur1 = JOptionPane.showInputDialog(null, "Entrez l'hauteur : ");
+        String largeur1 = JOptionPane.showInputDialog(null, "Entrez la largeur : ");
+        String hauteur2 = JOptionPane.showInputDialog(null, "Entrez l'hauteur2 : ");
+        String largeur2 = JOptionPane.showInputDialog(null, "Entrez la largeur2 : ");
+       float hauteur = Float.parseFloat(hauteur1);
+       float largeur = Float.parseFloat(largeur1);
+       float hauteurDeux = Float.parseFloat(hauteur2);
+       float largeurDeux = Float.parseFloat(largeur2);
+    float surfaceMural = hauteur * largeur;
+    float surfaceMural2 = hauteurDeux * largeurDeux; 
+   // DecimalFormat format = new DecimalFormat("0.00");
+    float nombredePapier = (surfaceMural+surfaceMural2)/surfaceRouleau;
+    JOptionPane.showMessageDialog(null, "  Le nombre de rouleaux de papier peint requis est :" + (nombredePapier));
+JOptionPane.showMessageDialog(null, "  Le nombre de rouleaux de papier peint requis est :" + Math.round(nombredePapier));
+         // Fermez le programme
+         System.exit(0);
+    }
+}
